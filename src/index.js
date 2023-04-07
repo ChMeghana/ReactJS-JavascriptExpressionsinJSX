ebe7a157a36a2028a96b21ffdf36fe66a27e02ca
Meghana Chelamalla <44540074+ChMeghana@users.noreply.github.com>
2023-04-06 23:20:11 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const num = 7;
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>My lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
